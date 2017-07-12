be0095778a37ed0b920c4ea04932c0fd7097a152
Shyam Chen <shyamchen1994@gmail.com>
2017-07-12 12:11:07 +0800

    4th Commit

--- test/test.js
+++ test/test.js
@@ -3,6 +3,8 @@ import { rollup } from 'rollup';
 
 import reshape from '../';
 
+import include from 'reshape-include';
+
 process.chdir(__dirname);
 
 const bundler = (entry, options) => rollup({ entry, plugins: [reshape(options)] });
@@ -26,31 +28,13 @@ describe('rollup-plugin-reshape', () => {
     });
   });
 
-  // it('should be able to use the plugins option', () => {
-  //   return bundler('fixtures/plugins/main.js', { plugins: [include()] })
-  //     .then(result => {
-  //       const { code } = result.generate({ format: 'iife', moduleName: 'posthtml' });
-  //       expect(code).toBeDefined();
-  //       expect(code).toMatch(/<p>Foo<\/p>/);
-  //       // expect(code).toMatch(/<p>Bar<\/p>/);
-  //   });
-  // });
-
-  // it('should be able to use the template option', () => {
-  //   return bundler('fixtures/template/main.js', { template: true })
-  //     .then(result => {
-  //       const { code } = result.generate({ format: 'iife', moduleName: 'posthtml' });
-  //       expect(code).toBeDefined();
-  //       expect(code).toMatch(/<p>\${ _.text }<\/p>/);
-  //   });
-  // });
-
-  // it('should be able to use the parser option', () => {
-  //   return bundler('fixtures/parser/main.js', { parser: sugarml() })
-  //     .then(result => {
-  //       const { code } = result.generate({ format: 'iife', moduleName: 'posthtml' });
-  //       expect(code).toBeDefined();
-  //       expect(code).toMatch(/<p>Foo<\/p>/);
-  //   });
-  // });
+  it('should be able to use the plugins option', () => {
+    return bundler('fixtures/plugins/main.js', { plugins: [include()] })
+      .then(result => {
+        const { code } = result.generate({ format: 'iife', moduleName: 'reshape' });
+        expect(code).toBeDefined();
+        expect(code).toMatch(/<p>Foo<\/p>/);
+        expect(code).toMatch(/<p>Bar<\/p>/);
+    });
+  });
 });
